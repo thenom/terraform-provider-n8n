d735096ff2837fc50cb41f74d9b0311efbcca83d
Simon Thorley <simon.thorley@zenbook.internal.thenom.co.uk>
2025-07-12 16:19:22 +0100

    Initial commit

---
+++ internal/provider/provider.go
@@ -0,0 +1,58 @@
+// Package provider for n8n
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ provider.Provider = &n8nProvider{}
+)
+
+// New is a helper function to simplify provider server and testing implementation.
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &n8nProvider{
+			version: version,
+		}
+	}
+}
+
+// n8nProvider is the provider implementation.
+type n8nProvider struct {
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
+}
+
+// Metadata returns the provider type name.
+func (p *n8nProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "n8n"
+	resp.Version = p.version
+}
+
+// Schema defines the provider-level schema for configuration data.
+func (p *n8nProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{}
+}
+
+// Configure prepares a HashiCups API client for data sources and resources.
+func (p *n8nProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+}
+
+// DataSources defines the data sources implemented in the provider.
+func (p *n8nProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return nil
+}
+
+// Resources defines the resources implemented in the provider.
+func (p *n8nProvider) Resources(_ context.Context) []func() resource.Resource {
+	return nil
+}
